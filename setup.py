587d77b8e239324dbd18dedb062306803f4fe374
Andres Pardini <unpardos@gmail.com>
2015-03-06 12:14:03 -0300

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-namespaced-cache',
+      version='0.1',
+      description='Cache with simple support for namespaced and cascade delete',
+      author='Andres Pardini',
+      author_email='unpardos@gmail.com',
+      url='https://github.com/pardo/namespaced-cache/',
+      packages=[],
+     )
